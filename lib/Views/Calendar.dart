f93f95c65b255c022afdc76a6b53dce92acfe960
robmroi03 <24611970+robmroi03@users.noreply.github.com>
2019-10-07 10:58:22 +0200

    Start with design of calendar.
    arrow.png will be updated to look better.

--- lib/Views/Calendar.dart
+++ lib/Views/Calendar.dart
@@ -5,9 +5,35 @@ class Calendar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new SafeArea(
-      child: new Center(
-        child: Text("CalendarView"),
-      ),
+      child: ListView(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container(
+                color: Color.fromRGBO(47, 109, 29, 1),
+                margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                width: 100,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Text("1"),
+                    ),
+                    Container(
+                      child: Image.asset("assets/arrow.png"),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Text("2"),
+                    )
+                  ],
+                ),
+
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 
