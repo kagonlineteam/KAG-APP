85a2303685bfaac4d5140719de964c5c5038a9c5
robmroi03 <24611970+robmroi03@users.noreply.github.com>
2019-10-30 21:47:04 +0100

    Add Design of the News view

--- lib/Views/News.dart
+++ lib/Views/News.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 class News extends StatelessWidget {
 
+  static const dateStyle = const TextStyle(fontSize: 25, color: Colors.white);
+  static const titleStyle = const TextStyle(fontSize: 35, fontWeight: FontWeight.bold, letterSpacing: 1);
+  static const descriptionStyle = const TextStyle(fontSize: 15);
+  static const subTextStyle = const TextStyle(fontSize: 10);
+  var usableWidth = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
+    var width = MediaQuery.of(context).size.width;
+    usableWidth = width - 132;
+
+    return Scaffold(
           appBar: AppBar(
             actions: <Widget>[
               Container(
@@ -27,11 +35,83 @@ class News extends StatelessWidget {
             ],
           )
           , body: SafeArea(
-          child: Center(
-            child: Text("Helllo"),
+          child: ListView(
+            children: <Widget>[
+              _generateRow(),
+              _generateRow(),
+              _generateRow()
+            ],
           )
       )
       );
+  }
+
+  Widget _generateRow() {
+    var title = "Title";
+    var descriptionText = "Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description ";
+    var date = "30.10.19";
+    var author = "RJipps";
+
+
+    return Container(
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(
+                  color: Color.fromRGBO(235, 235, 235, 1), width: 2))),
+      child: GestureDetector(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  color: Color.fromRGBO(200, 200, 200, 1),
+                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  width: 100,
+                  height: 100,
+
+                ),
+                Container(
+                  height: 100,
+                  width: usableWidth,
+                  margin: EdgeInsets.fromLTRB(0, 10, 10, 10),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: Text(title, style: titleStyle),
+                        alignment: Alignment.topLeft,
+                        height: 40,
+                      ),
+                      Container(
+                        child: Text(descriptionText,
+                            style: descriptionStyle,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 3),
+                        alignment: Alignment.topLeft,
+                        height: 55,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    child: Text(author, style: subTextStyle),
+                  ),
+                  Container(
+                    child: Text(date, style: subTextStyle),
+                  )
+                ],
+              ),
+              margin: EdgeInsets.fromLTRB(10, 0, 10, 5),
+            )
+          ],
+        )
+      ),
+    );
   }
 
 }
