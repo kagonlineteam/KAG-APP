645363b5df9edcc1de5fb605e0619d5744c338df
Felix <felixletsplay@outlook.de>
2020-10-10 16:07:04 +0200

    :recycle: Rebuild News View to use RessourceStream

--- lib/components/helpers.dart
+++ lib/components/helpers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../api/api_helpers.dart';
 
 const BorderSide splittingBorder      = const BorderSide(color: Color.fromRGBO(47, 109, 29, 1), width: 2);
 Container splittingContainer    = Container(margin: EdgeInsets.fromLTRB(10, 0, 10, 0), decoration: BoxDecoration(border: Border(top: splittingBorder)));
@@ -25,4 +26,47 @@ class ErrorTextHolder extends StatelessWidget {
   }
 
   String get error => _error;
+}
+
+class ResourceListBuilder<Resource> extends StatelessWidget {
+
+  final ListResource resource;
+  final ScrollController scrollController;
+  final Widget Function(List<Resource>, ScrollController scrollController) builder;
+  final String errorMessage;
+
+  ResourceListBuilder(resourceFunction, this.builder, {this.errorMessage = "Die Daten sind zur Zeit nicht verfügbar"}):
+      resource = resourceFunction(),
+      scrollController = ScrollController() {
+    scrollController.addListener(() {
+      if (scrollController.position.atEdge &&
+          scrollController.position.pixels != 0) {
+        resource.loadMore();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: RefreshIndicator(
+        onRefresh: () async => resource.reload(),
+        child: StreamBuilder(
+          stream: resource.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text(errorMessage));
+            } else if (!snapshot.hasData) {
+              return Stack(children: [
+                Center(child: Image.asset("assets/eule-rund.png")),
+                Center(child: CircularProgressIndicator())
+              ]);
+            } else {
+              return builder(snapshot.data, scrollController);
+            }
+          },
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/components/news.dart
@@ -0,0 +1,120 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../Views/News.dart';
+import '../api/api_models.dart';
+
+class ArticleCard extends StatelessWidget {
+  ArticleCard(this.article);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      margin: EdgeInsets.all(20),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        elevation: 1,
+        child: InkWell(
+          child: Stack(
+            children: [
+              ImageBox(article),
+              TitleBox(article)
+            ],
+          ),
+          splashColor: Color.fromRGBO(47, 109, 29, 1),
+          onTap: () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ArticleDetail(article))),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageBox extends StatelessWidget {
+  final Article article;
+
+  ImageBox(this.article);
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: article.hasImage,
+      child: Align(
+          alignment: Alignment.bottomCenter,
+          child: new Container(
+            height: 230,
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
+                    fit: BoxFit.fitWidth,
+                    alignment: FractionalOffset.center,
+                    image: CachedNetworkImageProvider("https://apiv2.kag-langenfeld.de/files/${article.imageID}")
+                )
+            ),
+          )
+      ),
+    );
+  }
+}
+
+class TitleBox extends StatelessWidget {
+  final Article article;
+  static const titleStyle       = const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white, letterSpacing: 0.5);
+
+  TitleBox(this.article);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(child: Align(
+      alignment: Alignment.bottomCenter,
+      child: SizedBox(
+        width: double.infinity,
+        child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color.fromRGBO(47, 47, 47, 0), Color.fromRGBO(47, 47, 47, 0.3),  Color.fromRGBO(47, 47, 47, 0.6), Color.fromRGBO(47, 47, 47, 0.8)],
+              ),
+            ),
+            padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Text(article.title, style: titleStyle, textAlign: TextAlign.left)
+            )
+        ),
+      ),
+    ));
+  }
+
+
+}
+
+class ArticleDetailWidget extends StatelessWidget {
+  ArticleDetailWidget(this.article);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    var htmlData = """<h1>${article.title}</h1><br><br> ${article.htmlBody != null ? article.htmlBody : ""}""";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("News"),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[Html(data: htmlData, onLinkTap: launch)],
+        ),
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+      ),
+    );
+  }
+
+}
