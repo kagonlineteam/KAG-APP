d149b6a2ce52f1a8ad5b548a78d01345633b36ad
Felix <felixletsplay@outlook.de>
2020-10-12 12:10:12 +0200

    :fire: Remove two-date mode in TerminList
    This is removed due to height constrains

--- lib/components/terminlist.dart
+++ lib/components/terminlist.dart
@@ -7,43 +7,22 @@ import '../api/api_models.dart';
 // Creates the Date Widget on the left
 class DateWidget extends StatelessWidget {
   DateWidget(Termin termin):
-      dateOneText = "${addLeadingZero(termin.startDatetime.day)}.${addLeadingZero(termin.startDatetime.month)}",
-      dateTwoText = "${addLeadingZero(termin.stopDatetime.day)}.${addLeadingZero(termin.stopDatetime.month)}";
-
+      dateText = "${addLeadingZero(termin.startDatetime.day)}.${addLeadingZero(termin.startDatetime.month)}";
   static const dateStyle = const TextStyle(fontSize: 20, color: Colors.white);
 
-  final String dateOneText, dateTwoText;
+  final String dateText;
 
   @override
   Widget build(BuildContext context) {
-    if (dateOneText == dateTwoText) {
-      return Container(
-        color: Theme.of(context).primaryColor,
-        child: Center(
-          child: Container(
-            child: Text(dateOneText, style: dateStyle),
-          ),
-        ),
-        width: 100,
-      );
-    }
     return Container(
-      color: Theme.of(context).primaryColor,
-      child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                child: Text(dateOneText, style: dateStyle),
-              ),
-              Container(
-                child: Image.asset("assets/arrow.png"),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                child: Text(dateTwoText, style: dateStyle),
-              )
-            ],
-          ),
+      color: Theme
+          .of(context)
+          .primaryColor,
+      child: Center(
+        child: Container(
+          child: Text(dateText, style: dateStyle),
+        ),
+      ),
       width: 100,
     );
   }
