a27303ceb9a58617a9865692c0830b6cc078ab8b
Felix <felixletsplay@outlook.de>
2020-11-01 22:48:14 +0100

    :bookmark: Bump Version to 1.1
    We plan to only apply fixes to the final 1.1 release
    from this commit on. Big new features will be in 1.2 release.

--- lib/components/menu.dart
+++ lib/components/menu.dart
@@ -10,7 +10,7 @@ class ExtraOptionsMenu extends StatelessWidget {
           showAboutDialog(
               context: context,
               applicationName: "KAG App",
-              applicationVersion: "1.0",
+              applicationVersion: "1.1",
               applicationLegalese: "Copyright KAG OnlineTeam 2019-2020\n\nThis App uses third-party software or other resources that may be distributed under different licenses. You can read them with the \"View Licenses\" button.",
               applicationIcon: Image.asset("assets/icon.png", width: 64,)
           );
