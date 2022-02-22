5af70c8978e00c2818c5bc925e54218cf6b3afd5
Felix <info@strifel.de>
2022-02-22 10:01:26 +0100

    :bug: Reset ios unread badge on app open
    Reset the unread badge to 0 on app open/foreground.

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,8 +6,15 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  // reset unread badge on app open
+  UIApplication.sharedApplication.applicationIconBadgeNumber = 0;
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // reset unread badge on app reopen
+    UIApplication.sharedApplication.applicationIconBadgeNumber = 0;
 }
 
 @end
