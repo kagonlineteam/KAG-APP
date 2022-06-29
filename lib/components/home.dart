41b07731cb3ceac8cce42078f42bb200adb18101
Vobistein <92033540+vobistein@users.noreply.github.com>
2022-06-29 12:11:37 +0200

    copied news into home

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -10,6 +11,7 @@ import '../api/api_models.dart';
 import 'helpers.dart';
 import 'menu.dart';
 import 'terminlist.dart';
+import '../api/api_raw.dart' as api_raw;
 
 /// Chooses if the Mobile or TabletWidget is needed
 class SurroundingWidget extends StatelessWidget {
@@ -124,7 +126,9 @@ class HomeList extends StatelessWidget {
         ShortcutsWidget(isTablet: isTablet),
         // Only show Impressum on web
         if (kIsWeb) splittingContainer,
-        if (kIsWeb) ImpressumWidget()
+        if (kIsWeb) ImpressumWidget(),
+        splittingContainer,
+        NewsWidget(),
     ],
     );
   }
@@ -222,6 +226,124 @@ class ShortcutsWidget extends StatelessWidget {
       ],
     );
   }
+
+}
+
+class NewsWidget extends StatelessWidget {
+  final News news;
+  final HomeScreenData homeScreenData;
+
+  const NewsWidget({Key key, this.news, this.homeScreenData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: ResourceListBuilder(
+        API.of(context).requests.getNewsArticles,
+          (data, controller) => ListView(
+            controller: controller,
+            children: [
+              Align(
+                alignment: AlignmentDirectional.topCenter,
+                child: Wrap(
+                  children: data.map<Widget>((article) => ArticleCard(article)).toList(),
+              ),
+            )
+          ],
+        )
+      )
+    );
+  }
+}
+
+class ImageBox extends StatelessWidget {
+  final Article article;
+
+  ImageBox(this.article);
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: article.hasImage,
+      child: Align(
+          alignment: Alignment.bottomCenter,
+          child: new Container(
+            height: 230,
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
+                    fit: BoxFit.fitWidth,
+                    alignment: FractionalOffset.center,
+                    image: CachedNetworkImageProvider("https://${api_raw.API}/files/${article.imageID}")
+                )
+            ),
+          )
+      ),
+    );
+  }
+}
+
+class ArticleCard extends StatelessWidget {
+  ArticleCard(this.article);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      margin: EdgeInsets.all(20),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        elevation: 1,
+        child: InkWell(
+          child: Stack(
+            children: [
+              ImageBox(article),
+              TitleBox(article)
+            ],
+          ),
+          splashColor: Color.fromRGBO(47, 109, 29, 1),
+          onTap: () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ArticleDetail(article))),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleBox extends StatelessWidget {
+  final Article article;
+  static const titleStyle       = const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white, letterSpacing: 0.5);
+
+  TitleBox(this.article);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(child: Align(
+      alignment: Alignment.bottomCenter,
+      child: SizedBox(
+        width: double.infinity,
+        child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color.fromRGBO(47, 47, 47, 0), Color.fromRGBO(47, 47, 47, 0.3),  Color.fromRGBO(47, 47, 47, 0.6), Color.fromRGBO(47, 47, 47, 0.8)],
+              ),
+            ),
+            padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Text(article.title, style: titleStyle, textAlign: TextAlign.left)
+            )
+        ),
+      ),
+    ));
+  }
+
 
 }
 
