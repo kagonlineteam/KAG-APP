4092291514b0fbcef7d2fb02f9a91fab8733a55a
Phantom061 <juliwe@gmx.de>
2022-07-02 15:32:58 +0200

    fix import errors

--- lib/push_notifications.dart
+++ lib/push_notifications.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:flutter/material.dart';
-import '../api/api_models.dart';
 import '../api/api.dart';
+import '../api/api_models.dart';
 import '../components/news.dart';
 
 import 'main.dart';
