affcb09f40b2dc0f499392809ff17b16af5fee09
strifel <info@strifel.de>
2022-07-09 13:02:32 +0200

    :bug: Fix WebMail
    This removes the old webmail integration and replaces it with one that
    uses a new endpoint that allows it to work now in web as well.

--- lib/Views/WebMail.dart
+++ lib/Views/WebMail.dart
@@ -25,21 +25,12 @@ class WebMail extends StatelessWidget {
         ],
       ),
       body: FutureBuilder(
-        future: Future.wait([API.of(context).requests.getUsername(), API.of(context).requests.getMailJWTToken()]),
+        future: API.of(context).requests.getWebmailHash(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            String username = snapshot.data[0];
-            String token = snapshot.data[1];
-            // This is a dirty hack as WebView does not yet support
-            // post data.
-            // https://stackoverflow.com/a/67948632/8314475 is the source of this
-            // code provided by Milad Ahmadi under CC BY-SA 4.0
-            final String postParam= "{Email: '$username', Login: '$username', Password: '$token'}";
-            final String loadWebmail="function post(path, params, method='post') {const form = document.createElement('form');form.method = method;form.action = path;for (const key in params) {if (params.hasOwnProperty(key)) {const hiddenField = document.createElement('input');hiddenField.type = 'hidden';hiddenField.name = key;hiddenField.value = params[key];form.appendChild(hiddenField);}}document.body.appendChild(form);form.submit();}post('https://webmail.kag-langenfeld.de/index.php?ExternalLogin', $postParam, method='post')";
-
             return WebView(
-              onWebViewCreated: (controller) => controller.runJavascript(loadWebmail),
               javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: "https://webmail.kag-langenfeld.de/?sso&hash=${snapshot.data}",
             );
           } else if (snapshot.hasError) {
             return ErrorTextHolder(snapshot.error);
